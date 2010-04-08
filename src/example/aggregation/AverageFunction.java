b1b27749d17bbf77161fcdb11f9a37289e13a275
wdidwlia@gmail.com <wdidwlia@gmail.com@8abe723d-0972-33be-60c4-43b964cbc01c>
2010-04-08 03:40:28 +0000

    display node

--- src/example/aggregation/AverageFunction.java
+++ src/example/aggregation/AverageFunction.java
@@ -70,9 +70,13 @@ public class AverageFunction extends SingleValueHolder implements CDProtocol {
 
             AverageFunction neighbor = (AverageFunction) peer
                     .getProtocol(protocolID);
+
+			System.out.print("before "+this.value+"\t"+neighbor.value);
+
             double mean = (this.value + neighbor.value) / 2;
             this.value = mean;
             neighbor.value = mean;
+			System.out.println("\tafter " + this.value+"\t" + neighbor.value);
         }
     }
 
