48fa144add22ffde79bf7adb694828d29c3b9696
wdidwlia <wdidwlia@8abe723d-0972-33be-60c4-43b964cbc01c>
2010-04-16 17:02:34 +0000

    Finished network initialization as Project 1 requires. Total number of nodes is 5000. The range from 0 to 19 represent 20 files, which is assigned to nodes as node value randomly. Occurrences of each file is from 5 to 50, that is, from 0.1 to 1 percentage of all nodes; It also is assigned randomly.

--- src/peersim/vector/GnutellaDistribution.java
+++ src/peersim/vector/GnutellaDistribution.java
@@ -5,12 +5,6 @@ import peersim.core.*;
 
 import java.util.Random;
 
-/**
- * Initializes a protocol vector with values in the range [{@value #PAR_MIN}, 
- * {@value #PAR_MAX}] (inclusive both ends), linearly increasing.
- * @see VectControl
- * @see peersim.vector
- */
 public class GnutellaDistribution extends VectControl
 {
 
@@ -18,119 +12,119 @@ public class GnutellaDistribution extends VectControl
 //Parameters
 //--------------------------------------------------------------------------
 
-/**
- * Upper end of the interval..
- * @config
- */
 //private static final String PAR_MAX = "max";
 
-/**
- * Lower end of the interval. Defaults to -{@value #PAR_MAX}.
- * @config
- */
 //private static final String PAR_MIN = "min";
 
 // --------------------------------------------------------------------------
 // Fields
 // --------------------------------------------------------------------------
 
-/** Minimum value */
+
 //private final Number min;
 
-/** Maximum value */
+
 //private final Number max;
 
-/** Step value */
+
 //private final double step;
 
-private final int file_number=20;
-private static double[] percentage=new double[file_number];
+private static final int file_number=20;
+private static int[] percentageRandom=new int[file_number];
+private static int[] filenumberActual=new int[file_number];
 
 
 // --------------------------------------------------------------------------
 // Initialization
 // --------------------------------------------------------------------------
 
-/**
- * Standard constructor that reads the configuration parameters.
- * Invoked by the simulation engine.
- * @param prefix the configuration prefix for this class
- */
 public GnutellaDistribution(String prefix)
 {
 	super(prefix);
-	
-	/*
-	// Read parameters based on type
-	if (setter.isInteger()) {
-		max=Long.valueOf(Configuration.getLong(prefix + "." + PAR_MAX));
-		min=Long.valueOf(Configuration.getLong(prefix + "." + PAR_MIN, 
-				-max.longValue()));
-		step= (max.longValue()-min.longValue())/
-			((double)(Network.size()-1));
-	} else { // we know it's double or float
-		max = new Double(Configuration.getDouble(prefix+"."+PAR_MAX));
-		min = new Double(Configuration.getDouble(prefix+"."+PAR_MIN, 
-				-max.doubleValue()));
-		step= (max.doubleValue()-min.doubleValue())/(Network.size()-1);
-	}
-	*/
 }
 
 // --------------------------------------------------------------------------
 // Methods
 // --------------------------------------------------------------------------
 
-/**
- * Initializes a protocol vector with values in the range [{@value #PAR_MIN}, 
- * {@value #PAR_MAX}] (inclusive both ends), linearly increasing.
- * @return always false
- */
 public boolean execute() {
 
 	Random r = new Random();
 
 	for(int a=0; a < file_number; a++)
 	{
-		percentage[a]=r.nextDouble()*0.9 + 0.1;
+		percentageRandom[a]=r.nextInt(45) + 5;
+		System.out.println(percentageRandom[a]);
 	}
 	
+	/*
 	if ( setter.isInteger() )
 	{
-		double n=Network.size();
+		int n=Network.size();
 		for(int i=0; i<n; ++i)
 		{
-			double p=0;
-			// we avoid the entire expression being cast to double
-			if(p < percentage[i] && i < 20)
+			int ran=r.nextInt(5000);
+			if( ran < 20 && filenumberActual[ran] < percentageRandom[ran])
 			{
-				ran=r.nextInt(20);
 				setter.set(i, ran);
-				p= (ran)/n; 
-
+				filenumberActual[ran]++;
 			}
 			else
 			{
-				setter.set(i, r.nextInt());
+				setter.set(i, r.nextInt()+20);
 			}
-			System.out.println(Math.round(i*step)+min.longValue());
+		}
+		for(int i=0; i<n; ++i) { System.out.println(getter.get(i));
 		}
 	}
-	/*
 	else
-	{
-		for(int i=0; i<Network.size(); ++i)
+	{*/
+		//	setter.set(i,i*step+min.doubleValue());
+		//	System.out.println(i*step+min.doubleValue());
+		int n=Network.size();
+		for(int i=0; i<n; ++i)
 		{
-			setter.set(i,i*step+min.doubleValue());
-			System.out.println(i*step+min.doubleValue());
+			int ran=r.nextInt(20);
+			if(ran < 20 && filenumberActual[ran] < percentageRandom[ran])
+			{
+				setter.set(i, (double) ran);
+				filenumberActual[ran]++;
+				//System.out.println("filenumberActual["+ran+"]: "+filenumberActual[ran]);
+				//System.out.println("percentageRandom["+ran+"]: "+percentageRandom[ran]);
+			}
+			else
+			{
+				ran=r.nextInt(4980)+20;
+				setter.set(i, (double)ran);
+	//			System.out.println(ran+" "+getter.get(i));
+			}
 		}
-	}
-	*/
+
+		/*
+		for(int i=0; i<file_number; i++)
+		{
+			if(i==1)
+			System.out.println(i + " " + filenumberActual[i]);
+		}
+		int[] cnt=new int[20];
+		for(int j=0; j<n; j++)
+		{
+
+		for(int i=0; i<n; ++i)
+		{
+			if(getter.get(i).intValue() == j) 
+				cnt[j]++;
+
+			//System.out.println(getter.get(i));
+		}
+		System.out.println("found " + cnt[j]);
+
+		}
+		*/
+	//}
 
 	return false;
 }
 
 
 }
-
-
