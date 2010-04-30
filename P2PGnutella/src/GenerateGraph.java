31fed96d0e34c8bbb6e70a6fd2852cd54f5cacb5
wdidwlia <wdidwlia@8abe723d-0972-33be-60c4-43b964cbc01c>
2010-04-30 17:15:14 +0000

    test the program to output the neigbour list of each node

--- P2PGnutella/src/GenerateGraph.java
+++ P2PGnutella/src/GenerateGraph.java
@@ -11,10 +11,10 @@ public class GenerateGraph {
 		graph.add(new Node(0,null));
 		
 		for(int i=1; i<=5000; i++){
+			System.out.print(i+"'s neighbors ");
 			graph.add(new Node(i, GenerateNeighbors()));
-			System.out.println(graph.size());
+			System.out.println();
 		}
-
 		
 	}//end constructor
 	
@@ -23,18 +23,36 @@ public class GenerateGraph {
 		List<Node> nodes = new ArrayList<Node>();
 		Random rand = new Random();
 		
+		/*
+		*/
 		if(graph.size() > 5){
 			int numNeighbors = 1+ rand.nextInt(4);
 			for(int i =0; i< numNeighbors; i++){
 				int neighbor=  rand.nextInt(graph.size());
 				nodes.add(graph.get(neighbor));
 			}
+
+			for(int j=0; j<nodes.size(); j++)
+			{
+				System.out.print(nodes.get(j).nodeId + " ");
+				if ( (j+1)%5==0)
+				System.out.println();
+			}
 		}
 		else{
+			//System.out.println();
 			int numNeighbors = rand.nextInt(graph.size());
 			for(int i =0; i< numNeighbors; i++){
 				int neighbor=  rand.nextInt(graph.size());
 				nodes.add(graph.get(neighbor));
+			}
+
+			//System.out.println();
+			for(int j=0; j<nodes.size(); j++)
+			{
+				System.out.print(nodes.get(j).nodeId + " ");
+				if ( (j+1)%5==0)
+				System.out.println();
 			}
 		}
 		return nodes;
